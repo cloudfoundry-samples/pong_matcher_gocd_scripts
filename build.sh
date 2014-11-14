da2dbd44be12361959ccae8c8ffd546aa717856c
Andrew Bruce <andrew@camelpunch.com>
2014-11-14 23:00:34 +0000

    Copy app into place instead of symlinking
    Symlinks don't work with Docker

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -ex
 
 docker_tag=docker.gocd.cf-app.com:5000/$APP_NAME
 dir=$APP_NAME
+cp -a ../app $dir/
 docker pull $docker_tag || true
 docker build $DOCKER_BUILD_OPTS -t $docker_tag $dir
 docker push $DOCKER_TAG
