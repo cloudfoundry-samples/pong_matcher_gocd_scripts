9031feb6b984ae2d1be7d9eb2dc0c6424e154399
Andrew Bruce <andrew@camelpunch.com>
2014-11-14 22:13:52 +0000

    Reduce number of env vars required

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
-docker pull $DOCKER_TAG || true
-docker build $DOCKER_BUILD_OPTS -t $DOCKER_TAG $DOCKER_DIR
+docker_tag=docker.gocd.cf-app.com:5000/$APP_NAME
+dir=$APP_NAME
+docker pull $docker_tag || true
+docker build $DOCKER_BUILD_OPTS -t $docker_tag $dir
 docker push $DOCKER_TAG
--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 set -xe
 
-docker pull $DOCKER_TAG
-docker run -e "CF_HOME=/pong_matcher" -t $DOCKER_TAG /bin/bash -c "\
+docker_tag=docker.gocd.cf-app.com:5000/$APP_NAME
+cf_space=$APP_NAME
+docker pull $docker_tag
+docker run -e "CF_HOME=/pong_matcher" -t $docker_tag /bin/bash -c "\
     cd pong_matcher
     cf api https://api.run.pivotal.io
     cf auth $CF_USERNAME $CF_PASSWORD &&
-    cf target -o $CF_ORG -s $CF_SPACE &&
-    cf push -n $HOSTNAME -d cfapps.io"
+    cf target -o $CF_ORG -s $cf_space &&
+    cf push -n $APP_NAME -d cfapps.io"
